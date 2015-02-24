1397dcc00bf2c358d740e61a60bafe9e4e680784
amorphic <jstewart101@gmail.com>
2015-02-24 17:41:31 +1100

    Inital checkin.

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Generate CV in various formats from a Markdown file.
+# Requires pandoc and wkhtmltopdf.
+
+FILENAME="james_stewart_cv"
+
+# html 
+echo "Building ${FILENAME}.html"
+/usr/bin/pandoc -s -S $FILENAME.md -c $FILENAME.css -o $FILENAME.html
+# docx 
+echo "Building ${FILENAME}.docx"
+/usr/bin/pandoc -s -S $FILENAME.md -c $FILENAME.css -o $FILENAME.docx
+# pdf
+echo "Building ${FILENAME}.pdf"
+/usr/bin/wkhtmltopdf $FILENAME.html $FILENAME.pdf
