0ff68343f7ea356e1b973b6cb559e896622f792b
James Stewart <james@amorphitec.io>
2019-01-06 21:53:24 +1100

    Update markdown and build script for new pandoc

--- build.sh
+++ build.sh
@@ -7,12 +7,12 @@ FILENAME="james_stewart_cv"
 
 # html 
 echo "Building ${FILENAME}.html"
-/usr/bin/pandoc -s -S $FILENAME.md -c $FILENAME.css -o $FILENAME.html
+/usr/bin/pandoc -f markdown+smart -s --metadata title="James Anthony Stewart" $FILENAME.md -c $FILENAME.css -o $FILENAME.html
 cp $FILENAME.html index.html
 
 # docx 
 echo "Building ${FILENAME}.docx"
-/usr/bin/pandoc -s -S $FILENAME.md -c $FILENAME.css -o $FILENAME.docx
+/usr/bin/pandoc -f markdown+smart -s --metadata title="James Anthony Stewart" $FILENAME.md -c $FILENAME.css -o $FILENAME.docx
 
 # pdf
 echo "Building ${FILENAME}.pdf"
